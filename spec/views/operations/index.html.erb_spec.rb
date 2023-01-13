fac147f7894aebe89c81fae4641fa0646d4aa636
mullenlucas <mullenlucas@gmail.com>
2023-01-13 13:16:43 -0300

    fix rubocop offenses

--- spec/views/operations/index.html.erb_spec.rb
+++ spec/views/operations/index.html.erb_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "operations/index", type: :view do
+RSpec.describe 'operations/index', type: :view do
   before(:each) do
     assign(:operations, [
-      Operation.create!(),
-      Operation.create!()
-    ])
+             Operation.create!,
+             Operation.create!
+           ])
   end
 
-  it "renders a list of operations" do
+  it 'renders a list of operations' do
     render
     cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
   end
