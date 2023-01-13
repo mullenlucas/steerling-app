3e4dfa9fb376e7d98549934262241457cd97d1bf
mullenlucas <mullenlucas@gmail.com>
2023-01-13 12:51:36 -0300

    update routes and configs

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_13_151244) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_13_154043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "categories_operations", id: false, force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "operation_id", null: false
+    t.index ["category_id", "operation_id"], name: "index_categories_operations_on_category_id_and_operation_id"
+    t.index ["operation_id", "category_id"], name: "index_categories_operations_on_operation_id_and_category_id"
   end
 
   create_table "operations", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_operations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,8 +43,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_13_151244) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
+  add_foreign_key "operations", "users"
 end
