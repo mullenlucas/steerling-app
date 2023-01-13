b83ff2a706a017cd46afb11709fbe224066c55d2
mullenlucas <mullenlucas@gmail.com>
2023-01-13 18:41:36 -0300

    update app models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+  has_and_belongs_to_many :operations, dependent: :destroy
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
