420792992c9696259965eda1323e9e053552388a
mullenlucas <mullenlucas@gmail.com>
2023-01-13 21:53:59 -0300

    fix rubocop offenses

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,8 +2,8 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :manage, Group, user: user
-    can :manage, Charge, user: user
+    can(:manage, Group, user:)
+    can :manage, Charge, user:
     # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
